0c9db613b515448e6ca9abc07314d72d21ffe37d
awicenec <awicenec@users.noreply.github.com>
2022-12-25 04:31:06 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="awicenec"
+original_name="pvplotter"
+original_urlname="PVplotter"
+original_description="Awesome pvplotter created by awicenec"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv pvplotter $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
